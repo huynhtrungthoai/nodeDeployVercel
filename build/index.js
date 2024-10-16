27fd36f4ee45e45b0f8f0946b469bbf9b1d2f012
Thoai Huynh <huynhtrungthoai@gmail.com>
2024-10-16 20:20:07 +0700

    Update build 1

---
+++ build/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Express on Vercel'));
+app.listen(8080, () => console.log('Server ready on port 8080.'));
+module.exports = app;
+//# sourceMappingURL=index.js.map
